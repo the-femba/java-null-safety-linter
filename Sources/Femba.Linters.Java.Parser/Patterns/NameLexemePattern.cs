70dd7275b47713586f1f3cb6d849ad447ee3a3e4
Roman S <ua.romansyslikov@gmail.com>
2022-02-20 02:19:53 +0300

    Create NameLexemePattern.cs

---
+++ Sources/Femba.Linters.Java.Parser/Patterns/NameLexemePattern.cs
@@ -0,0 +1,15 @@
+using System.Text.RegularExpressions;
+using Femba.Linters.Java.Parser.Interfaces;
+
+namespace Femba.Linters.Java.Parser.Patterns;
+
+public sealed class NameLexemePattern : ILexemePattern
+{
+	private readonly Regex _regex = new(@"^[\w]+$");
+	
+	public bool IsMatch(string matcher)
+		=> _regex.IsMatch(matcher);
+
+	public string Match(string matcher)
+		=> matcher;
+}
