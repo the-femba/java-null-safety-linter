ff0aa942cb8a5b9a657eeb95f01d523bb1f2e142
Roman S <ua.romansyslikov@gmail.com>
2022-02-20 02:19:44 +0300

    Create TypeLexemePattern.cs

---
+++ Sources/Femba.Linters.Java.Parser/Patterns/TypeLexemePattern.cs
@@ -0,0 +1,16 @@
+using Femba.Linters.Java.Parser.Interfaces;
+
+namespace Femba.Linters.Java.Parser.Patterns;
+
+public sealed class TypeLexemePattern : ILexemePattern
+{
+	public bool IsMatch(string matcher)
+	{
+		throw new NotImplementedException();
+	}
+
+	public string Match(string matcher)
+	{
+		throw new NotImplementedException();
+	}
+}
