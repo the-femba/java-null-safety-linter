f559ba6c705d0ded592fdd84f4606aade9564b9d
Roman S <ua.romansyslikov@gmail.com>
2022-02-20 02:19:59 +0300

    Update TokenType.cs

--- Sources/Femba.Linters.Java.Parser/Enums/TokenType.cs
+++ Sources/Femba.Linters.Java.Parser/Enums/TokenType.cs
@@ -5,20 +5,31 @@ namespace Femba.Linters.Java.Parser.Enums;
 /// </summary>
 public enum TokenType
 {
+	Unknown = -1,
+	
 	/// <summary>
 	/// Типы, например: string, text, Point.
 	/// </summary>
 	Type,
+	
 	/// <summary>
 	/// Имена функций и переменных, например: myVar1, myFunc1.
 	/// </summary>
 	Name,
+	
 	/// <summary>
 	/// Символы языка, например: @, {, }, (, ), +, -, /, *, =, >, ;. И множество других.
 	/// </summary>
 	Symbol,
+	
 	/// <summary>
 	/// Константы, например: "text", 23.45, 23.
 	/// </summary>
-	Literal
+	Literal,
+	
+	/// <summary>
+	/// Ключевые слова, например: if, else, switch, return, break, this, new.
+	/// Void, string и другие ключевые слова, которые являются типами, относятся к типам.
+	/// </summary>
+	Keyword,
 }
