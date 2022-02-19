3662b3da6b766ed2984a9b984e1b25f8a2a6c228
Roman S <ua.romansyslikov@gmail.com>
2022-02-20 02:42:53 +0300

    Update TypeLexemePattern.cs

--- Sources/Femba.Linters.Java.Parser/Patterns/TypeLexemePattern.cs
+++ Sources/Femba.Linters.Java.Parser/Patterns/TypeLexemePattern.cs
@@ -1,16 +1,14 @@
+using System.Text.RegularExpressions;
 using Femba.Linters.Java.Parser.Interfaces;
 
 namespace Femba.Linters.Java.Parser.Patterns;
 
 public sealed class TypeLexemePattern : ILexemePattern
 {
-	public bool IsMatch(string matcher)
-	{
-		throw new NotImplementedException();
-	}
+	// TODO: Refactored regex.
+	private readonly Regex _regex = new(@"^([\w]+)((\s+|\s+[\w]+)|)$");
+	
+	public bool IsMatch(string matcher) => _regex.IsMatch(matcher);
 
-	public string Match(string matcher)
-	{
-		throw new NotImplementedException();
-	}
+	public string Match(string matcher) => _regex.Match(matcher).Groups[1].Value;
 }
