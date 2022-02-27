0e1ffe11922e0755aa144da4eead701b513e05ba
Roman S <ua.romansyslikov@gmail.com>
2022-02-28 00:09:32 +0300

    Create NullLiteralTokenPattern.cs

---
+++ Sources/Femba.Linters.Java/Patterns/NullLiteralTokenPattern.cs
@@ -0,0 +1,10 @@
+using System.Text.RegularExpressions;
+using Femba.Linters.Java.Parser.Common;
+using Femba.Linters.Java.Parser.Enums;
+
+namespace Femba.Linters.Java.Parser.Patterns;
+
+public sealed class NullLiteralTokenPattern : TokenRegexPattern
+{
+	public NullLiteralTokenPattern() : base(TokenType.Literal, new Regex("^(n|nu|nul|null)$")) { }
+}
