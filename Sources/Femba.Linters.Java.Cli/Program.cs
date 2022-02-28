979ef2e5f57de0e26a18df454a2bcf1db2d6505b
Roman S <ua.romansyslikov@gmail.com>
2022-02-28 05:19:35 +0300

    Add Cli

--- Sources/Femba.Linters.Java.Cli/Program.cs
+++ Sources/Femba.Linters.Java.Cli/Program.cs
@@ -1,9 +1,43 @@
-namespace Femba.Linters.Java.Cli;
+﻿
+using Femba.Linters.Java.Parser.Common;
+using Femba.Linters.Java.Parser.Features;
+using Femba.Linters.Java.Parser.Interfaces;
+using Femba.Linters.Java.Parser.Patterns;
+using Newtonsoft.Json;
 
-public static class Program
+var path = args[0];
+
+var text = File.ReadAllText(path);
+
+text = new Formatter().Format(text);
+
+var tokens = new Lexer(text, patterns: new HashSet<ITokenPattern>
 {
-	public static void Main(string[] args)
-	{
-		
-	}
-}
+	new NumberLiteralPattern(),
+	new NullLiteralTokenPattern(),
+	new StringLiteralTokenPattern(),
+	new CharLiteralTokenPattern(),
+	new KeywordPattern(),
+	new NamePattern(),
+	new SymbolPattern(),
+	new TypePattern()
+}).LexToEnd();
+
+var nodes = new Parser(tokens.ToList(), patterns: new List<INodePattern>
+{
+	new LiteralNodePattern(),
+	new FunctionNodePattern(),
+	new VariableInvokeNodePattern(),
+	new FunctionInvokeNodePattern(),
+	new VariableAssignmentNodePattern()
+}).ParseToEnd();
+
+var results = new Analyzer(nodes, features: new List<IFeature>
+{
+	new NullSafeFeature()
+}).Analyze();
+
+text = JsonConvert.SerializeObject(results, Formatting.Indented);
+
+File.WriteAllText(path + ".analyze.json", text);
+Console.WriteLine(text);
